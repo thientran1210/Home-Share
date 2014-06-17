c9c1b39f654e90d43a2b2c22b90e25671caa90c6
thientran1210 <thientran1210@yahoo.com>
2014-06-17 23:46:46 +1000

    Test

---
+++ Home Share/src/home/share/Member.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package home.share;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author toshiba
+ */
+public class Member {
+    
+    private String name;
+    private double debtMoney;
+    private double ownMoney;
+    private ArrayList<Transaction> transaction;
+    
+    
+}
